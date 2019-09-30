24456920110458ccadb98985bb4a2b4b799bb35b
dimapirus <dimapirus@gmail.com>
2019-09-30 15:50:00 +0300

    1. main page all

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var x = document.getElementById('circle_second');
+var leftSecond = x.offsetLeft;
+var dSecond = document.getElementById('d_second');
+dSecond.style.left = (leftSecond - 10)  + "px";
+
+var y = document.getElementById('circle_third');
+var leftThird = y.offsetLeft;
+var dThird = document.getElementById('d_third');
+dThird.style.left = (leftThird - 10)  + "px";
+
+var z = document.getElementById('circle_four');
+var leftFour = z.offsetLeft;
+var dFour = document.getElementById('d_four');
+dFour.style.left = (leftFour - 10)  + "px";
+
+var v = document.getElementById('circle_five');
+var leftFive = v.offsetLeft;
+var dFive = document.getElementById('d_five');
+dFive.style.left = (leftFive - 10)  + "px";
+
+var dFive = document.getElementById('d_six');
+dFive.style.right = 0  + "px";
